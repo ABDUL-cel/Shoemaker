757e72bba8eb12b6411172c0d3f4d07e5184c421
ABDUL-cel <khalidabdulazeez19@gmail.com>
2026-09-02 03:09:44 +0100

    Update design.js

--- Backend/routes/design.js
+++ Backend/routes/design.js
@@ -3,7 +3,7 @@ const { nanoid } = require('nanoid');
 const multer = require('multer');
 const { DesignRequest } = require('../db');
 const { uploadBufferToCloudinary } = require('../cloudinary');
-const { requireAdminKey } = require('middleware/middleware');
+const { requireAdminKey } = require('../middleware');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage(), limits: { fileSize: 8 * 1024 * 1024 } });
